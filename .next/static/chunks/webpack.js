7c0a35ac2b7f706c426748ca1df22dc19671e112
Alatise <fayobio17@gmail.com>
2023-02-06 11:16:28 +0100

    profile and other pages in progress

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "273b910064d75ab2"; }
+/******/ 		__webpack_require__.h = function() { return "4bda44f036bced90"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
