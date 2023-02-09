3709885fa02cabe9f08dbdc5781ba7ab48b33aa0
Alatise <fayobio17@gmail.com>
2023-02-09 18:46:32 +0100

    Profile page updated to the latest features

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5e478d9af4208ea9"; }
+/******/ 		__webpack_require__.h = function() { return "421b182ddd55d629"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
